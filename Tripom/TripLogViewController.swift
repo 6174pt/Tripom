8db31d5af6023f12f30f4d46a645b2db97a9eba9
Honoka Nishiyama <hono.nish6555@gmail.com>
2024-06-09 21:47:42 +0900

    旅記録画面レスポンシブ対応,ScrollViewへの移行2

--- Tripom/TripLogViewController.swift
+++ Tripom/TripLogViewController.swift
@@ -10,85 +10,77 @@ import RealmSwift
 
 class TripLogViewController: UIViewController, UICollectionViewDataSource {
     
-//    @IBOutlet var destinationRequirementLabel: UILabel!
-//    @IBOutlet var transportationRequirementLabel: UILabel!
-//    @IBOutlet var costRequirementLabel: UILabel!
-//    @IBOutlet var curfewRequirementLabel: UILabel!
-//    @IBOutlet var tripCommentTextView: UITextView!
-    
+    //    ScrollView
     let scrollView: UIScrollView = UIScrollView()
     let stackView: UIStackView = UIStackView()
+    
+    //    旅条件
     let requirementsView: UIView = UIView()
-    
-    let requirementsCommentView: UIView = UIView()
-    let requirementsCommentLabel: UILabel = UILabel()
-    
+    //    旅条件-タイトルセクション
+    let requirementsTitleView: UIView = UIView()
+    let requirementsTitleLabel: UILabel = UILabel()
+    //    旅条件-目的地セクション
     let destinationRequirementsView: UIView = UIView()
     let destinationImageView: UIImageView = UIImageView()
     let destinationLabel: UILabel = UILabel()
     var destinationRequirementLabel: UILabel = UILabel()
-    
+    //    旅条件-交通手段セクション
     let transportationRequirementsView: UIView = UIView()
     let transportationImageView: UIImageView = UIImageView()
     let transportationLabel: UILabel = UILabel()
     var transportationRequirementLabel: UILabel = UILabel()
-    
+    //    旅条件-費用セクション
     let costRequirementsView: UIView = UIView()
     let costImageView: UIImageView = UIImageView()
     let costLabel: UILabel = UILabel()
     var costRequirementLabel: UILabel = UILabel()
-    
+    //    旅条件-帰宅時間セクション
     let curfewRequirementsView: UIView = UIView()
     let curfewImageView: UIImageView = UIImageView()
     let curfewLabel: UILabel = UILabel()
     var curfewRequirementLabel: UILabel = UILabel()
     
+    //    コメント
     let commentView: UIView = UIView()
-    
+    //    コメント-タイトルセクション
     let tripCommentTitleView: UIView = UIView()
     let tripCommentTitleLabel: UILabel = UILabel()
-    
+    //    コメント-コメント本文セクション
     let tripCommentView: UIView = UIView()
     let tripCommentTextView: UITextView = UITextView()
     
+    //    写真
     let photosView: UIView = UIView()
-    
+    //    写真-タイトルセクション
     let tripPhotoTitleView: UIView = UIView()
     let tripPhotoTitleLabel: UILabel = UILabel()
+    //    写真-写真
     let tripPhotoView: UIView = UIView()
-//    let tripPhotoCollectionView: UICollectionView = UICollectionView()
     
     var index: Int = 0
     let realm = try! Realm()
     var tripLogs: Results<TripLog>!
     var tripLog: TripLog!
     
-//    @IBOutlet var tripPhotoCollectionView: UICollectionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewdidload")
         
         self.tabBarController?.tabBar.isHidden = true
         
         let screenWidth = UIScreen.main.bounds.size.width
         let screenHeight = UIScreen.main.bounds.size.height
         
+//        scrollViewの設置
         scrollView.frame = self.view.frame
         scrollView.contentSize = CGSize(width:screenWidth, height:screenHeight)
         view.addSubview(scrollView)
-        
+//        stackViewの設置
         stackView.axis = .vertical
         stackView.alignment = .fill
         stackView.distribution = .fill
         stackView.spacing = 0
         stackView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.addSubview(stackView)
-        
-        // requirementsView1の設定
-//        requirementsView.backgroundColor = .red
-        requirementsView.heightAnchor.constraint(equalToConstant: screenHeight / 3).isActive = true
-        stackView.addArrangedSubview(requirementsView)
         
         let requirementsViewHeight = screenHeight / 3
         let photosViewHeight = screenHeight / 2
@@ -101,35 +93,39 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         let sectionLeftRightMargin = (view.frame.size.width * (1.0 - sectionWidthRation)) / 2 + 20
         let sectionTopBottomMargin = sectionHeight / 6
         
+        //    旅条件Viewの設置
+        requirementsView.heightAnchor.constraint(equalToConstant: screenHeight / 3).isActive = true
+        stackView.addArrangedSubview(requirementsView)
+        
+        //        旅条件View下線
         let requirementsViewBottomBorder = CALayer()
         requirementsViewBottomBorder.frame = CGRect(x: 5, y: requirementsViewHeight, width: view.frame.size.width - 10, height:1.0)
         requirementsViewBottomBorder.backgroundColor = UIColor.systemGray4.cgColor
         requirementsView.layer.addSublayer(requirementsViewBottomBorder)
         
-        //        コメントセクション
-        requirementsCommentView.frame=CGRect(x: 10, y: sectionTopBottomMargin, width: sectionWidth, height: sectionHeight)
-        requirementsView.addSubview(requirementsCommentView)
+        //        旅条件-タイトルセクション
+        requirementsTitleView.frame=CGRect(x: 10, y: sectionTopBottomMargin, width: sectionWidth, height: sectionHeight)
+        requirementsView.addSubview(requirementsTitleView)
         
-        //        コメントセクション-コメント
-        requirementsCommentLabel.frame=CGRect(x: 0, y: 0, width: sectionWidth, height: sectionHeight)
-        requirementsCommentLabel.text = "旅条件"
-        requirementsCommentLabel.textAlignment = .left
-        requirementsCommentLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        requirementsCommentLabel.adjustsFontSizeToFitWidth = true
-        requirementsCommentView.addSubview(requirementsCommentLabel)
+        //        旅条件-タイトルセクション-タイトル
+        requirementsTitleLabel.frame=CGRect(x: 0, y: 0, width: sectionWidth, height: sectionHeight)
+        requirementsTitleLabel.text = "旅条件"
+        requirementsTitleLabel.textAlignment = .left
+        requirementsTitleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        requirementsTitleLabel.adjustsFontSizeToFitWidth = true
+        requirementsTitleView.addSubview(requirementsTitleLabel)
         
-//        目的地セクション
+        //        旅条件-目的地セクション
         destinationRequirementsView.frame=CGRect(x: 20, y: sectionTopBottomMargin * 2 + sectionHeight, width: sectionWidth, height: sectionHeight)
-//        destinationRequirementsView.backgroundColor = UIColor.systemGray
         requirementsView.addSubview(destinationRequirementsView)
         
-//        目的地セクション-アイコン
+        //        旅条件-目的地セクション-アイコン
         destinationImageView.frame=CGRect(x: 0, y: 0, width: sectionHeight * 2 / 3, height: sectionHeight * 2 / 3)
         destinationImageView.image = UIImage(systemName: "mappin.and.ellipse")
         destinationImageView.tintColor = UIColor.black
         destinationRequirementsView.addSubview(destinationImageView)
         
-//        目的地セクション-目的地
+//        旅条件-目的地セクション-目的地
         destinationLabel.frame=CGRect(x: 0, y: sectionHeight * 2 / 3, width: sectionHeight * 2 / 3, height: sectionHeight / 3)
         destinationLabel.text = "目的地"
         destinationLabel.textColor = UIColor.systemGray2
@@ -137,25 +133,24 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         destinationLabel.adjustsFontSizeToFitWidth = true
         destinationRequirementsView.addSubview(destinationLabel)
         
-//        目的地セクション-目的地条件
+//        旅条件-目的地セクション-目的地条件
         destinationRequirementLabel.frame=CGRect(x: requirementsViewHeight / 12 + 20, y: 0, width: sectionWidth - sectionHeight, height: sectionHeight)
         destinationRequirementLabel.adjustsFontSizeToFitWidth = true
         destinationRequirementLabel.font = UIFont.systemFont(ofSize: 18.0)
         destinationRequirementLabel.textAlignment = .left
         destinationRequirementsView.addSubview(destinationRequirementLabel)
         
-//        交通手段セクション
+//        旅条件-交通手段セクション
         transportationRequirementsView.frame=CGRect(x: 20, y: sectionTopBottomMargin * 3 + sectionHeight * 2, width: sectionWidth, height: sectionHeight)
-//        transportationRequirementsView.backgroundColor = UIColor.systemGray2
         requirementsView.addSubview(transportationRequirementsView)
         
-//        交通手段セクション-アイコン
+//        旅条件-交通手段セクション-アイコン
         transportationImageView.frame=CGRect(x: 0, y: 0, width: sectionHeight * 2 / 3, height: sectionHeight * 2 / 3)
         transportationImageView.image = UIImage(systemName: "tram")
         transportationImageView.tintColor = UIColor.black
         transportationRequirementsView.addSubview(transportationImageView)
         
-//        交通手段セクション-交通手段
+//        旅条件-交通手段セクション-交通手段
         transportationLabel.frame=CGRect(x: 0, y: sectionHeight * 2 / 3, width: sectionHeight * 2 / 3, height: sectionHeight / 3)
         transportationLabel.text = "交通手段"
         transportationLabel.textColor = UIColor.systemGray2
@@ -163,25 +158,24 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         transportationLabel.adjustsFontSizeToFitWidth = true
         transportationRequirementsView.addSubview(transportationLabel)
         
-//        交通手段セクション-交通手段条件
+//        旅条件-交通手段セクション-交通手段条件
         transportationRequirementLabel.frame=CGRect(x: requirementsViewHeight / 12 + 20, y: 0, width: sectionWidth - sectionHeight, height: sectionHeight)
         transportationRequirementLabel.adjustsFontSizeToFitWidth = true
         transportationRequirementLabel.font = UIFont.systemFont(ofSize: 18.0)
         transportationRequirementLabel.textAlignment = .left
         transportationRequirementsView.addSubview(transportationRequirementLabel)
         
-//        費用セクション
+//        旅条件-費用セクション
         costRequirementsView.frame=CGRect(x: 20, y: sectionTopBottomMargin * 4 + sectionHeight * 3, width: sectionWidth, height: sectionHeight)
-//        costRequirementsView.backgroundColor = UIColor.systemGray3
         requirementsView.addSubview(costRequirementsView)
         
-//        費用セクション-アイコン
+//        旅条件-費用セクション-アイコン
         costImageView.frame=CGRect(x: 0, y: 0, width: sectionHeight * 2 / 3, height: sectionHeight * 2 / 3)
         costImageView.image = UIImage(systemName: "yensign")
         costImageView.tintColor = UIColor.black
         costRequirementsView.addSubview(costImageView)
         
-//        費用セクション-費用
+//        旅条件-費用セクション-費用
         costLabel.frame=CGRect(x: 0, y: sectionHeight * 2 / 3, width: sectionHeight * 2 / 3, height: sectionHeight / 3)
         costLabel.text = "費用"
         costLabel.textColor = UIColor.systemGray2
@@ -189,25 +183,24 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         costLabel.adjustsFontSizeToFitWidth = true
         costRequirementsView.addSubview(costLabel)
         
-//        費用セクション-費用条件
+//        旅条件-費用セクション-費用条件
         costRequirementLabel.frame=CGRect(x: requirementsViewHeight / 12 + 20, y: 0, width: sectionWidth - sectionHeight, height: sectionHeight)
         costRequirementLabel.adjustsFontSizeToFitWidth = true
         costRequirementLabel.font = UIFont.systemFont(ofSize: 18.0)
         costRequirementLabel.textAlignment = .left
         costRequirementsView.addSubview(costRequirementLabel)
         
-//        帰宅時間セクション
+//        旅条件-帰宅時間セクション
         curfewRequirementsView.frame=CGRect(x: 20, y: sectionTopBottomMargin * 5 + sectionHeight * 4, width: sectionWidth, height: sectionHeight)
-//        curfewRequirementsView.backgroundColor = UIColor.systemGray4
         requirementsView.addSubview(curfewRequirementsView)
         
-//        帰宅時間セクション-アイコン
+//        旅条件-帰宅時間セクション-アイコン
         curfewImageView.frame=CGRect(x: 0, y: 0, width: sectionHeight * 2 / 3, height: sectionHeight * 2 / 3)
         curfewImageView.image = UIImage(systemName: "clock")
         curfewImageView.tintColor = UIColor.black
         curfewRequirementsView.addSubview(curfewImageView)
         
-//        帰宅時間セクション-帰宅時間
+//        旅条件-帰宅時間セクション-帰宅時間
         curfewLabel.frame=CGRect(x: 0, y: sectionHeight * 2 / 3, width: sectionHeight * 2 / 3, height: sectionHeight / 3)
         curfewLabel.text = "帰宅時間"
         curfewLabel.textColor = UIColor.systemGray2
@@ -215,29 +208,28 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         curfewLabel.adjustsFontSizeToFitWidth = true
         curfewRequirementsView.addSubview(curfewLabel)
         
-//        帰宅時間セクション-帰宅時間条件
+//        旅条件-帰宅時間セクション-帰宅時間条件
         curfewRequirementLabel.frame=CGRect(x: requirementsViewHeight / 12 + 20, y: 0, width: sectionWidth - sectionHeight, height: sectionHeight)
         curfewRequirementLabel.adjustsFontSizeToFitWidth = true
         curfewRequirementLabel.font = UIFont.systemFont(ofSize: 18.0)
         curfewRequirementLabel.textAlignment = .left
         curfewRequirementsView.addSubview(curfewRequirementLabel)
         
-        // commentViewの設定
-//        commentView.backgroundColor = .blue
+        // コメントViewの設置
         commentView.heightAnchor.constraint(equalToConstant: screenHeight / 3).isActive = true
         stackView.addArrangedSubview(commentView)
         
+//        コメントView下線
         let commentViewBottomBorder = CALayer()
         commentViewBottomBorder.frame = CGRect(x: 5, y: requirementsViewHeight, width: view.frame.size.width - 10, height:1.0)
         commentViewBottomBorder.backgroundColor = UIColor.systemGray4.cgColor
         commentView.layer.addSublayer(commentViewBottomBorder)
         
-        //        コメント題名セクション
+        //        コメント-タイトルセクション
         tripCommentTitleView.frame=CGRect(x: 10, y: sectionTopBottomMargin, width: sectionWidth, height: sectionHeight)
-//        tripCommentTitleView.backgroundColor = UIColor.gray
         commentView.addSubview(tripCommentTitleView)
         
-        //        コメント題名セクション-コメント
+        //        コメント-タイトルセクション-タイトル
         tripCommentTitleLabel.frame=CGRect(x: 0, y: 0, width: sectionWidth, height: sectionHeight)
         tripCommentTitleLabel.text = "コメント"
         tripCommentTitleLabel.textAlignment = .left
@@ -245,26 +237,24 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         tripCommentTitleLabel.adjustsFontSizeToFitWidth = true
         tripCommentTitleView.addSubview(tripCommentTitleLabel)
         
-        //        コメント本文セクション
+        //        コメント-コメントセクション
         tripCommentView.frame=CGRect(x: 20, y: sectionTopBottomMargin * 2 + sectionHeight, width: commentSectionWidth, height: requirementsViewHeight - sectionHeight - sectionTopBottomMargin * 3)
         tripCommentView.backgroundColor = UIColor.systemGray
         commentView.addSubview(tripCommentView)
         
-        //        コメント本文セクション-コメント本文
+        //        コメント-コメントセクション-コメント
         tripCommentTextView.frame=CGRect(x: 0, y: 0, width: commentSectionWidth, height: requirementsViewHeight - sectionHeight - sectionTopBottomMargin * 3)
         tripCommentView.addSubview(tripCommentTextView)
         
-        // photosViewの設定
-//        photosView.backgroundColor = .green
+//        写真Viewの設置
         photosView.heightAnchor.constraint(equalToConstant: screenHeight / 2).isActive = true
         stackView.addArrangedSubview(photosView)
         
-        //        写真タイトルセクション
+        //        写真-写真タイトルセクション
         tripPhotoTitleView.frame=CGRect(x: 10, y: sectionTopBottomMargin, width: sectionWidth, height: sectionHeight)
-//        tripPhotoTitleView.backgroundColor = UIColor.gray
         photosView.addSubview(tripPhotoTitleView)
         
-        //        写真タイトルセクション-写真タイトル
+        //        写真-写真タイトルセクション-タイトル
         tripPhotoTitleLabel.frame=CGRect(x: 0, y: 0, width: sectionWidth, height: sectionHeight)
         tripPhotoTitleLabel.text = "写真"
         tripPhotoTitleLabel.textAlignment = .left
@@ -272,33 +262,23 @@ class TripLogViewController: UIViewController, UICollectionViewDataSource {
         tripPhotoTitleLabel.adjustsFontSizeToFitWidth = true
         tripPhotoTitleView.addSubview(tripPhotoTitleLabel)
         
-        //        写真セクション
+        //        写真-写真セクション
         tripPhotoView.frame=CGRect(x: 0, y: sectionTopBottomMargin * 2 + sectionHeight, width: photosSectionWidth, height: photosViewHeight - sectionHeight - sectionTopBottomMargin * 3)
 //        tripPhotoView.backgroundColor = UIColor.systemGray
         photosView.addSubview(tripPhotoView)
         
-        //        写真セクション-写真
-        
-//        if let layout = tripPhotoCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-//            layout.estimatedItemSize = .zero // 自動サイズ調整を無効にする
-//        }
+        //        写真-写真セクション-写真
         let layout = UICollectionViewFlowLayout()
         layout.minimumInteritemSpacing = 10
         layout.scrollDirection = .horizontal
         layout.itemSize = CGSize(width: (self.view.frame.width - 30) / 2, height:(self.view.frame.width - 30 ) * 1.5 / 2 )
         layout.sectionInset = UIEdgeInsets(top: 0,left: 10,bottom: 0,right: 10)
-//        tripPhotoCollectionView.collectionViewLayout = layout
-        
         let tripPhotoCollectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: photosSectionWidth, height: photosViewHeight - sectionHeight - sectionTopBottomMargin * 3), collectionViewLayout: layout)
-//        tripPhotoCollectionView.backgroundColor = UIColor.blue
         tripPhotoCollectionView.dataSource = self
         tripPhotoCollectionView.register(UINib(nibName: "TripLogPhotosCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "TripLogPhotosCollectionViewCell")
-//        tripPhotoCollectionView.frame=CGRect(x: 0, y: 0, width: commentSectionWidth, height: requirementsViewHeight - sectionHeight - sectionTopBottomMargin * 3)
         tripPhotoView.addSubview(tripPhotoCollectionView)
         
-        
-        
-        // stackViewの制約を設定
+        // stackViewの制約
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
             stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
